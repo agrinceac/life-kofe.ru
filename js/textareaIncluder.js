627cc5737a6a549af7475e316d62b14707f6f094
agrinceac <sashagrinceac1>
2017-12-12 18:55:49 +0200

    Изменил код textareajsincluder

--- js/textareaIncluder.js
+++ js/textareaIncluder.js
@@ -1,4 +1,4 @@
 window.addEventListener("load", function(){
-    document.body.innerHTML =
-        document.body.innerHTML.replace(/textareajsincluderalias/g, "textarea");
+    // document.body.innerHTML = document.body.innerHTML.replace(/textareajsincluderalias/g, "textarea");
+    $('textareajsincluderalias')[0].outerHTML.replace("textareajsincluderalias","textarea");
 });
